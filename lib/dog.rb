e18a9f1b34b23faca59317ac6e21022441cf7630
Kristian Joos <kjoos22@gmail.com>
2020-09-20 17:32:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+#Class to represent a dog
+class Dog
+  
+  #method to allow Dog to .bark
+  def bark
+    puts "Woof!"
+  end
+  
+  #method to allow Dog to .sit
+  def sit
+    puts "The Dog is sitting"
+  end
+end
